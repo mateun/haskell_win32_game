972b434e7fc36aeb49630f1dd5828f7d302c5ae1
Martin Gruscher <m.gruscher@ttech.at>
2018-01-01 07:17:59 +0100

    replace tabs with spaces

--- src/c/winstuff.c
+++ src/c/winstuff.c
@@ -17,36 +17,36 @@ void clearScreen()
 void InitGL(HWND hWnd) 
 {
   PIXELFORMATDESCRIPTOR pfd =
-    {
-      sizeof(PIXELFORMATDESCRIPTOR),
-      1,
-      PFD_DRAW_TO_WINDOW | PFD_SUPPORT_OPENGL | PFD_DOUBLEBUFFER,    //Flags
-      PFD_TYPE_RGBA,            //The kind of framebuffer. RGBA or palette.
-      32,                        //Colordepth of the framebuffer.
-      0, 0, 0, 0, 0, 0,
-      0,
-      0,
-      0,
-      0, 0, 0, 0,
-      24,                        //Number of bits for the depthbuffer
-      8,                        //Number of bits for the stencilbuffer
-      0,                        //Number of Aux buffers in the framebuffer.
-      PFD_MAIN_PLANE,
-      0,
-      0, 0, 0
-    };
+  {
+    sizeof(PIXELFORMATDESCRIPTOR),
+    1,
+    PFD_DRAW_TO_WINDOW | PFD_SUPPORT_OPENGL | PFD_DOUBLEBUFFER,    //Flags
+    PFD_TYPE_RGBA,            //The kind of framebuffer. RGBA or palette.
+    32,                        //Colordepth of the framebuffer.
+    0, 0, 0, 0, 0, 0,
+    0,
+    0,
+    0,
+    0, 0, 0, 0,
+    24,                        //Number of bits for the depthbuffer
+    8,                        //Number of bits for the stencilbuffer
+    0,                        //Number of Aux buffers in the framebuffer.
+    PFD_MAIN_PLANE,
+    0,
+    0, 0, 0
+  };
 
-		HDC ourWindowHandleToDeviceContext = GetDC(hWnd);
+  HDC ourWindowHandleToDeviceContext = GetDC(hWnd);
 
-		int  letWindowsChooseThisPixelFormat;
-		letWindowsChooseThisPixelFormat = ChoosePixelFormat(ourWindowHandleToDeviceContext, &pfd); 
-		SetPixelFormat(ourWindowHandleToDeviceContext,letWindowsChooseThisPixelFormat, &pfd);
+  int  letWindowsChooseThisPixelFormat;
+  letWindowsChooseThisPixelFormat = ChoosePixelFormat(ourWindowHandleToDeviceContext, &pfd); 
+  SetPixelFormat(ourWindowHandleToDeviceContext,letWindowsChooseThisPixelFormat, &pfd);
 
-		HGLRC ourOpenGLRenderingContext = wglCreateContext(ourWindowHandleToDeviceContext);
-		wglMakeCurrent (ourWindowHandleToDeviceContext, ourOpenGLRenderingContext);
+  HGLRC ourOpenGLRenderingContext = wglCreateContext(ourWindowHandleToDeviceContext);
+  wglMakeCurrent (ourWindowHandleToDeviceContext, ourOpenGLRenderingContext);
 
-		
-		printf("OPENGL VERSION: %s\n", (char*)glGetString(GL_VERSION) );
+
+  printf("OPENGL VERSION: %s\n", (char*)glGetString(GL_VERSION) );
 
 }
 
